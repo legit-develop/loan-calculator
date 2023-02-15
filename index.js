ab7e36275e8878c5c6e353706333de3310a4f606
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-02-15 16:21:57 +0200

    add new element functionality

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+calculateLoan();
+
 function calculateLoan() {
   const loanAmountValue = document.getElementById("loan-amount").value;
   const interestRateValue = document.getElementById("interest-rate").value;
   const monthsToPayValue = document.getElementById("months-to-pay").value;
 
   interest = (loanAmountValue * (interestRateValue * 0.01)) / monthsToPayValue;
-  monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(2);
+  monthlyPayment = (loanAmountValue / monthsToPayValue + interest).toFixed(0);
   monthPay = document.getElementById("payment");
-  monthPay.innerHTML = `Monthly Payment: ${monthlyPayment}`;
+  monthPay.innerHTML = `Monthly Payment: R ${monthlyPayment}`;
+  totalAmountPayed = (monthlyPayment * monthsToPayValue).toFixed(0);
+  totalAmount = document.getElementById("total-amount");
+  totalAmount.innerText = `Total Amount Payed: R ${totalAmountPayed}`;
+  bankMoney = (totalAmountPayed - loanAmountValue).toFixed(0);
+  bankProfitEl = document.getElementById("bank-profit");
+  bankProfitEl.innerText = `Bank Profit: R ${bankMoney}`;
 }
